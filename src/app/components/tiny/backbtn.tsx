4e24c8720af4cef2f9b28b3f249fd8df6c762ec9
gelthemu <gelthemu@gmail.com>
2025-01-29 23:06:41 +0300

    update

--- src/app/components/tiny/backbtn.tsx
+++ src/app/components/tiny/backbtn.tsx
@@ -6,7 +6,8 @@ export default function BackBtn() {
   return (
     <button
       onClick={() => window.history.back()}
-      className="inline-flex items-center"
+      className="inline-flex items-center px-1 py-2.5 text-sm text-gray/80 dark:text-light/60 font-medium"
+      aria-label="Go Back"
     >
       <MoveLeft className="mr-2 h-4 w-4" />
       Back
