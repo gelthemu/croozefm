15cc0ecb01471cc15208f9528527302568e7df9c
gelthemu <gelthemu@gmail.com>
2025-02-21 20:14:37 +0300

    auto

--- src/app/c/shows/components/record-player.tsx
+++ src/app/c/shows/components/record-player.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { toast } from "react-toastify";
 import type { Show } from "@/types/show";
 import { useMiniPlayer } from "@/app/context/mini-player-context";
