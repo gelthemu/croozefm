a00573da5ca2b6e3f090f8ccdac9a1376cd2ce80
gelthemu <gelthemu@gmail.com>
2025-02-22 22:11:26 +0300

    download-btn

--- src/app/components/announcement/components/download-btn.tsx
+++ src/app/components/announcement/components/download-btn.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { toast } from "react-toastify";
 import { StreamButton } from "../../stream/stream-btn";
 import useDownloader from "react-use-downloader";
 
 export default function MixtapeDownloadBtn() {
+  const [isDownloading, setIsDownloading] = useState(false);
+  const isActive = isDownloading;
+
   const { download } = useDownloader({
     mode: "no-cors",
     credentials: "include",
@@ -14,46 +17,60 @@ export default function MixtapeDownloadBtn() {
     },
   });
 
-  const handleDownload = async () => {
-    try {
-      const fileUrl =
-        "https://croozefm.blob.core.windows.net/audio/weekend-love.mp3";
+  const getFilename = (url: string) => {
+    const match = url.match(/\/([^\/]+)\.mp3$/);
+    return match ? `${match[1].toLowerCase()}.mp3` : "cfm-weekly-mixtape.mp3";
+  };
 
-      const proxyUrl = await toast.promise(
-        fetch(`/api/download?url=${encodeURIComponent(fileUrl)}`, {
-          credentials: "include", // Include cookies for authentication
+  const handleDownload = async () => {
+    if (isDownloading) return;
+
+    try {
+      setIsDownloading(true);
+
+      const MIXTAPE_URL =
+        "https://croozefm.com/wp-content/uploads/2025/02/Crooze-FM-Weekly-Mixtapes-DJ-Emma-Vol-1-19th-Feb-2025_01.mp3";
+
+      const MIXTAPE_NAME = getFilename(MIXTAPE_URL);
+
+      const PROXY_URL = await toast.promise(
+        fetch(`/api/download?url=${encodeURIComponent(MIXTAPE_URL)}`, {
+          credentials: "include",
         }),
         {
           pending: "Downloading...",
-          error: "Error downloading",
+          error: "Download failed!!!",
           success: {
             render() {
-              return "Your download will start shortly!";
+              return "Download completed!";
             },
-            autoClose: 4000,
+            autoClose: 5000,
           },
         }
       );
 
-      const filename =
-        "Crooze-FM-Weekly-Mixtapes-DJ-Emma-Vol-1-19th-Feb-2025_01.mp3";
-
-      const response = await proxyUrl;
-      const audioUrl = await response.url;
-      download(audioUrl, filename);
+      const response = await PROXY_URL;
+      const AUDIO_URL = await response.url;
+      download(AUDIO_URL, MIXTAPE_NAME);
     } catch (error) {
       console.error(error);
       toast.error("Download failed. Please try again.");
+    } finally {
+      setIsDownloading(false);
     }
   };
 
   return (
-
-     <StreamButton
-     text={"Download Mixtape"}
-     className="px-4 py-2 bg-[#3eac75] dark:bg-[#3eac75]/60"
-     onClick={handleDownload}
-     isDownload={true}
-   />
+    <StreamButton
+      text={isDownloading ? "Downloading..." : "Download Mixtape"}
+      className={`px-4 py-2 ${
+        isActive
+          ? "bg-gray/80 dark:bg-gray/100"
+          : "bg-[#3eac75] dark:bg-[#3eac75]/60"
+      } `}
+      onClick={handleDownload}
+      isDownload={true}
+      isActive={isActive}
+    />
   );
 }
