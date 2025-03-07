d70f186ca5dbb6c86b844f33a8cd0940100fc473
gelthemu <gelthemu@gmail.com>
2025-03-07 22:32:20 +0300

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,13 +1,21 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://croozefm.geltaverse.com',
-    generateRobotsTxt: true,
-    changefreq: 'daily',
-    priority: 0.7,
-    exclude: ['/api/*'],
-    robotsTxtOptions: {
-      policies: [
-        { userAgent: '*', allow: '/' },
-      ],
-    },
-  };
+  siteUrl: process.env.SITE_URL || "https://croozefm.geltaverse.com",
+  generateRobotsTxt: true,
+  changefreq: "daily",
+  priority: 0.7,
+  exclude: ["/", "/api/*"],
+  additionalPaths: async () => {
+    return [
+      {
+        loc: "/home",
+        changefreq: "daily",
+        priority: 1.0,
+        lastmod: new Date().toISOString(),
+      },
+    ];
+  },
+  robotsTxtOptions: {
+    policies: [{ userAgent: "*", allow: "/" }],
+  },
+};
