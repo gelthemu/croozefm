2b5714865aeeff4b6e10f2a17d844d77dc595b0e
Gel <gelthemu@gmail.com>
2025-04-07 16:50:52 +0300

    update

--- src/app/policies/[policy]/page.tsx
+++ src/app/policies/[policy]/page.tsx
@@ -49,7 +49,7 @@ export default async function PolicyPage({
 
   return (
     <div className="w-full max-w-3xl mx-auto p-1">
-      <div className="relative p-6 bg-gray/20 dark:bg-gray/50 rounded-sm shadow-xl z-0 border-y-2 border-red">
+      <div className="relative z-0">
         <div className="py-4 mb-10 flex flex-col border-b border-gray/40 dark:border-light/20">
           <div>
             <h1 className="text-3xl pb-4 text-red _912cfm">{policy.title}</h1>
