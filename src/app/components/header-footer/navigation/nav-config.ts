7e0527e0f7b163662f26255270f790ea1fb1a83d
gelthemu <gelthemu@gmail.com>
2025-04-09 12:39:02 +0300

    auto

--- src/app/components/header-footer/navigation/nav-config.ts
+++ src/app/components/header-footer/navigation/nav-config.ts
@@ -1,6 +1,6 @@
 export const navLinks = [
   { id: "home", name: "Home", href: "/home" },
-  { id: "shows", name: "Shows", href: "/shows" },
+  { id: "mixtapes", name: "Mixtapes", href: "/c/mixtapes" },
   { id: "presenters", name: "Presenters", href: "/i/team" },
   { id: "news", name: "News", href: "/news/" },
 ];
